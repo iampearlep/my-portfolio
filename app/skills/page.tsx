df0406d6ff81d89e0a5e086dc933d061a45cefc5
Princess Enuma <87270940+iampearlep@users.noreply.github.com>
2024-07-19 23:08:16 +0100

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -62,7 +62,7 @@ export default function Skills() {
 
     const animation = {
         initial: {opacity: 0, scale:0.5},
-        enter:  (i: number) => ({opacity:1, scale:1, transition: {duration: 0.5, ease: easeInOut,  delay: 0.5 * i}})
+        enter:  (i: number) => ({opacity:1, scale:1, transition: {duration: 0.5, ease: easeInOut,  delay: 0.38 * i}})
       }
 
   return (
