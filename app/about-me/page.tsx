7a50ce8cdebe3c648e2fb6d66d1dcce32beb6725
iampearlep <damilolaprincess4@gmail.com>
2024-07-20 23:19:33 +0100

    feat: update about-me page

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -2,25 +2,80 @@ import Link from "next/link";
 import { IoIosArrowRoundBack } from "react-icons/io";
 
 export default function AboutMe() {
-
-
   return (
     <main className="md:h-[100vh] w-full flex flex-col  gap-y-7 md:gap-y-20 justify-center items-center text-neutral-50 mx-auto py-10 md:py-0">
-     <h2 className="text-4xl font-semibold">About me</h2>
+      <h2 className="text-4xl font-semibold">About me</h2>
 
-   <div className="flex flex-col text-center justify-center items-center w-11/12 md:w-7/12 text-base leading-loose">
-   <p>Hello, I&#39;m Princess Enuma, a front-end developer with a passion for building beautiful and functional user interfaces,
-   I&#39;m interested in user experience, accessibility and web animations.
-   I&#39;ve always been fascinated by the way technology can shape user experiences. That&#39;s why I love turning designs into interactive, user-friendly websites.
-   I&#39;m always eager to learn new technologies and contribute to innovative projects.
-   Witnessing my code come to life as a user-friendly website is incredibly rewarding. 
-   When I&#39;m not coding, you&#39;ll likely find me lost in the world of music, exploring new genres, or unwinding with a good anime series.
-   </p> 
-   </div>
+      <div className="flex flex-col text-center justify-center items-center w-11/12 md:w-7/12 text-[0.92rem] leading-loose md:text-base md:leading-loose">
+        <p>
+          Hello, I&#39;m Princess Enuma,{" "}
+          <span className="font-light text-neutral-500"> a self-motivated</span>{" "}
+          front-end developer{" "}
+          <span className="font-light text-neutral-500">
+            with a passion for
+          </span>{" "}
+          building beautiful, brilliant,{" "}
+          <span className="font-light text-neutral-500">
+            and functional user interfaces{" "}
+          </span>
+          using frontend technologies,{" "}
+          <span className="font-light text-neutral-500">
+            I&#39;m interested in
+          </span>{" "}
+          user experience,{" "}
+          <span className="font-light text-neutral-500">accessibility and</span>{" "}
+          web animations.{" "}
+          <span className="font-light text-neutral-500">
+            I&#39;ve always been{" "}
+          </span>
+          fascinated{" "}
+          <span className="font-light text-neutral-500">
+            by the way technology can shape
+          </span>{" "}
+          user experiences.
+          <span className="font-light text-neutral-500">
+            That&#39;s why I
+          </span>{" "}
+          love turning designs into{" "}
+          <span className="font-light text-neutral-500">interactive, and</span>{" "}
+          user-friendly websites.{" "}
+          <span className="font-light text-neutral-500">
+            I&#39;m always eager
+          </span>{" "}
+          to learn new technologies
+          <span className="font-light text-neutral-500">
+            {" "}
+            and contribute
+          </span>{" "}
+          to innovative projects.{" "}
+          <span className="font-light text-neutral-500">
+            Witnessing my code come to life
+          </span>{" "}
+          as a user-friendly website is <span className="font-light text-neutral-500">incredibly rewarding</span>. When I&#39;m not
+          coding,{" "}
+          <span className="font-light text-neutral-500">
+            {" "}
+            you&#39;ll likely find me
+          </span>{" "}
+          lost in the world of music,
+          <span className="font-light text-neutral-500">
+            {" "}
+            exploring new genres
+          </span>
+          , or unwinding with a good anime series.
+        </p>
+      </div>
 
-   <div>
-    <Link href="/" className="flex flex-row justify-center items-center gap-x-1"> <IoIosArrowRoundBack />Go back </Link>
-   </div>
+      <div>
+        <Link
+          href="/"
+          className="flex flex-row justify-center items-center gap-x-1"
+        >
+          {" "}
+          <IoIosArrowRoundBack />
+          Go back{" "}
+        </Link>
+      </div>
     </main>
   );
 }
